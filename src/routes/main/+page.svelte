a3ad0adbe9697fd5a71e3b892297913f84eb227c
Tanish <tanishgambhir55@gmail.com>
2025-06-19 23:56:03 +0530

    Developed a menu with different options to choose from and made  UI for the To-do list

--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -1,7 +1,18 @@
 <script>
+    import ToDo from './ToDo.svelte';
     let time = $state(25 * 60);
     let isRunning = false;
     let timer;
+    let isDropDownOpen = $state(false);
+    let name = "Tanish";
+    let showList = $state(false);
+    let quotes = [
+        "The only way to do great work is to love what you do. - Steve Jobs",
+        "Life is what happens when you're busy making other plans. - John Lennon",
+        "Get busy living or get busy dying. - Stephen King",
+        "You have within you right now, everything you need to deal with whatever the world can throw at you. - Brian Tracy",
+        "Believe you can and you're halfway there. - Theodore Roosevelt"
+    ];
 
     function startTimer() {
         if (isRunning) return;
@@ -30,25 +41,62 @@
     }
 
     function toggleTheme() {
+        // Code to be added for toggling theme
+    }
+
+    function toggleDropDown() {
+        isDropDownOpen = !isDropDownOpen;
+    }
+
+    function toggleTodoList() {
+        showList = !showList;
     }
 
 </script>
 
-<div class="flex justify-end px-5">
-    <button class="border-2">Toggle Screen</button>
-</div>
+<div class="flex justify-end px-5 gap-x-5">
+    <div class="relative">
+    <button onclick={toggleDropDown} class="border-2">Open menu</button>
 
-<div class="space-y-20 py-50 flex flex-col items-center justify-center">
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="text-8xl">{Math.floor(time/60)}:{String(time%60).padStart(2,'0')}</h1>
+    {#if isDropDownOpen}
+        <div class="absolute top-10 w-45 bg-blue-200 text-lg text-black rounded-sm shadow-lg z-1">
+			<button class="block px-4 py-2 hover:bg-gray-100 w-full">To-do List</button>
+			<button class="block px-4 py-2 hover:bg-gray-100 w-full">Shuffle Quote</button>
+			<button class="block px-4 py-2 hover:bg-gray-100 w-full">Enable Bunny</button>
+			<button class="block px-4 py-2 hover:bg-gray-100 w-full">Settings</button>
+        </div>
+    {/if}
     </div>
 
-    <div class="space-x-20">
-        <button onclick={startTimer} class="border-2">Start</button>
-        <button onclick={pauseTimer} class="border-2">Pause</button>
-        <button onclick={resetTimer} class="border-2">Reset</button>
+    <button class="border-2">Toggle Theme</button>
+</div>
+
+<div class="absolute top-[350px] left-25 z-1">
+    <ToDo/>
+</div>
+
+<div class="relative">
+    <div>
+        <div class="text-3xl text-center absolute w-full">Good Day, {name}</div>
+    </div>
+
+    <div>
+        <div><!--To be added--></div>
+    </div>
+
+    <div class="space-y-20 py-60 flex flex-col items-center justify-center">
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="text-7xl">{Math.floor(time/60)}:{String(time%60).padStart(2,'0')}</h1>
+        </div>
+
+        <div class="space-x-20">
+            <button onclick={startTimer} class="border-2">Start</button>
+            <button onclick={pauseTimer} class="border-2">Pause</button>
+            <button onclick={resetTimer} class="border-2">Reset</button>
+        </div>
     </div>
 </div>
+
 
 <style>
 
---
+++ src/routes/main/ToDo.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<div>
+    <form>
+        <h1>What needs to be done?</h1>
+        <input type="text" name="addTask" id="addTask" placeholder="Add a new task" class="border-2 p-2 rounded-md w-full mb-4">
+        <button class="border-5 h-full w-full">Add</button>
+        <div id="taskList">
+            <ul>
+                <li class="flex justify-between items-center border-b-2 py-2">
+                    <input type="checkbox" name="" id="" class="border-none rounded-2xl focus:outline-none outline-none active:outline-none focus:ring-0 ring-0 active:ring-0">
+                    <span>Sample Task 1</span>
+                </li>
+                <li class="flex justify-between items-center border-b-2 py-2">
+                    <span>Sample Task 2</span>
+                    <button class="border-2 px-2">Done</button>
+                </li>
+            </ul>
+        </div>
+        <div class="flex justify-between mt-2">
+            <button class="border-4 mt-5">Check all</button>
+            <button class="border-4 mt-5">Remove all</button>
+        </div>
+    </form>
+</div>
+
+<style>
+
+</style>
